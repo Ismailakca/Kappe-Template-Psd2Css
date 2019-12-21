7611cd091e23f71862f8425211f5c9374598720f
ismailakca <ismaillakkca@gmail.com>
2019-12-22 02:03:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#sidebarCollapse').on('click',function(){
+      $('#sidebar').toggleClass('active');
+    });
+    $('#sidebarCollapse2').on('click',function(){
+      $('#sidebar2').toggleClass('active');
+    });
+  });
+
